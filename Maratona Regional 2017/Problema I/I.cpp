dafdeb3ae2ca9d2c69d9320afe55cad652b5a1b8
jjaneto <jjaneto@dcomp.ufs.br>
2017-09-19 10:30:35 -0300

    #Fixing two

--- Maratona Regional 2017/Problema I/I.cpp
+++ Maratona Regional 2017/Problema I/I.cpp
@@ -1,112 +1,48 @@
-#include <queue>
-#include <cmath>
-#include <cstdio>
-#include <vector>
-#include <iostream>
-#include <cstring>
-#include <map>
-
+//Autor: Welerson
+//Complexidade: O(V + E)
+#include<bits/stdc++.h>
 using namespace std;
 
-typedef pair<int, int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
+int dev[11234], visited[11234];
+int n, c, a, b, w;
 
-vector<vii> adj;
-vi howMuch, visited, grau, levels;
-int n, c;
+vector<pair<int, int> > adjList[11234];
 
+int ans = 0;
 
-int dfs(int u, int length){
-  visited[u] = true;
-  int ret = 0;
-  for(int j = 0; j < (int) adj[u].size(); j++){
-    ii v = adj[u][j];
-    if(!visited[v.first]){
-      int gogo = dfs(v.first, length + v.second);
-      //printf("v %d me retornou %d\n", v.first, gogo);
-      ret += gogo;
+void dfs(int u, int pai, int posJ){
+  visited[u] = 1;
+  for(int j = 0; j < (int)adjList[u].size(); j++){
+    int v = adjList[u][j].first;
+    if(!visited[v]){
+      dfs(v, u, j);
+      dev[u]+=dev[v];
     }
   }
-  //printf("length ate %d eh %d\n", u, length);
-  if(howMuch[u]){
-    //printf("%d deve %d\n", u, howMuch[u]);
-    if(c > howMuch[u]){
-      //printf("so preciso de uma passada, vou add apenas %d\n", length * 2);
-      ret = ret + (length * 2);
-    }else{
-      //printf("preciso de %d passadas, vou add ", (howMuch[u] / c) + (howMuch[u] % c != 0));
-      int div = howMuch[u] / c;
-      int add = (2 * length * (div + (howMuch[u] % c != 0)));
-      //printf("%d\n", add);
-      ret = ret + add;
-    }
-  }
-  return ret;
-}
-
-
-void bfs(){
-  queue<int> q;
-  for(int i = 1; i <= n; i++){
-    if(grau[i] == 1){
-      //printf("coloquei %d\n", i);
-      q.push(i);
-    }
-  }
-  
-  while(!q.empty()){
-    int u = q.front(); q.pop();
-    if(u == 1) continue;
-    //printf("to em %d, ele deve %d\n", u, howMuch[u]);
-    for(int j = 0; j < (int) adj[u].size(); j++){
-      int v = adj[u][j].first;
-      int maxload = (int) abs(c - howMuch[v]);
-      if(v == 1) continue;
-      if((levels[v] == levels[u] - 1) && maxload){//se eh o meu pai e ele pode receber
-        //printf("tirei %d de %d, que tinha %d e agora tem %d, para colocar em %d, que tinha %d e agora tem %d\n", min(maxload, howMuch[u]), u, howMuch[u], howMuch[u] - min(maxload, howMuch[u]), v, howMuch[v], howMuch[v] + min(maxload, howMuch[u]));
-        howMuch[v] += min(maxload, howMuch[u]);
-        howMuch[u] -= min(maxload, howMuch[u]);
-        q.push(v);
-      }      
-    }
-  }
-}
-
-void findLevels(int u, int pai, int level){
-  levels[u] = level;
-  for(int i = 0; i < (int) adj[u].size(); i++){
-    int v = adj[u][i].first;
-    if(v != pai){
-      findLevels(v, u, level + 1);
-    }
+  if(pai != -1){
+    int aux = (dev[u] % c) ? dev[u]/c + 1 : dev[u]/c;
+    ans += aux * adjList[pai][posJ].second;
   }
 }
 
 int main(){
-  while(scanf("%d %d", &n, &c) != EOF){
-    howMuch.resize(n + 1);
-    adj.assign(n + 1, vii());
-    grau.assign(n + 1, 0);
-    levels.assign(n + 1, 0);
-    visited.assign(n + 1, 0);
-    for(int i = 1; i <= n; i++){
-      scanf("%d", &howMuch[i]);
-    }
-    for(int i = 0; i < n - 1; i++){
-      int u, v, w; scanf("%d %d %d", &u, &v, &w);
-      adj[u].push_back(ii(v, w));
-      adj[v].push_back(ii(u, w));
-      grau[u]++, grau[v]++;
-    }
-    findLevels(1, -1, 0);
-    bfs();
-    /*puts("atual estado de divida");
-      for(int i = 1; i <= n; i++){
-      printf("%d ", howMuch[i]);
-      }
-      puts("");*/
-    printf("%d\n", dfs(1, 0));
+  ios::sync_with_stdio(false);
+  cin.tie(0); cout.tie(0);
+  cin>>n>>c;
+  
+  for(int i = 0; i < n; i++)
+		cin>>dev[i];
+  
+  for(int i = 0; i < n-1; i++){
+    cin>>a>>b>>w;
+    a--;b--;
+    adjList[a].push_back({b, w});
+    adjList[b].push_back({a, w});
   }
+  
+  dfs(0, -1, -1);
+  
+  cout<<2*ans<<endl;
+  
   return 0;
 }
